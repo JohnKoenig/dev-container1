ac7a270588e06ff03772922c1c65811b8758db7c
John Koenig <koenigj3@gmail.com>
2024-01-20 04:41:56 +0000

    enum and first func

--- main.swift
+++ main.swift
@@ -1,4 +1,20 @@
 let a = 10
-let b = 75
+let b = 77
 let c: Int = a+b
-print("The sum of a and b is \(c).")
+
+enum Status {
+    case even
+    case odd
+}
+
+func checkEvenOrOdd(sum: Int) -> Status {
+    if c.isMultiple(of: 2) {
+        return .even
+    } else {
+        return Status.odd
+    }
+}
+
+let result = checkEvenOrOdd(sum: c)
+
+print("The sum of a and b is \(c) and it is \(result).")
